9ae82b363fa004d643a79e367a0d8e0189c32c30
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-23 19:19:47 +0600

    password bcrypt

--- controller/registration.js
+++ controller/registration.js
@@ -1,40 +1,48 @@
 const UserList = require('../models/userSchema')
+const bcrypt = require('bcrypt');
 
-function registration(req,res){
+
+function registration(req, res) {
     const { fristname, lastname, email, telephone, address, city, postcode, divison, district, password } = req.body
     console.log(req.body);
 
-    if(!fristname){
-      return  res.send({error: 'Fristname is required'})
+    if (!fristname) {
+        return res.send({ error: 'Fristname is required' })
     }
-    if(!lastname){
-      return  res.send({error: 'Lastname is required'})
+    if (!lastname) {
+        return res.send({ error: 'Lastname is required' })
     }
-    if(!email){
-      return  res.send({error: 'Email is required'})
-    }
-    if(!telephone){
-      return  res.send({error: 'Telephone is required'})
-    }
-    if(!password){
-      return  res.send({error: 'Password is required'})
+    if (!email) {
+        return res.send({ error: 'Email is required' })
     }
 
-    const users = new UserList({
-        fristname,
-        lastname,
-        email,
-        telephone,
-        address,
-        city,
-        postcode,
-        divison,
-        district,
-        password
 
-    })
-    users.save();
-    res.send(users) ;
+    if (!telephone) {
+        return res.send({ error: 'Telephone is required' })
+    }
+    if (!password) {
+        return res.send({ error: 'Password is required' })
+    }
+
+    bcrypt.hash(password, 10, function (err, hash) {
+        const users = new UserList({
+            fristname,
+            lastname,
+            email,
+            telephone,
+            address,
+            city,
+            postcode,
+            divison,
+            district,
+            password:hash
+
+        })
+        users.save();
+        res.send(users);
+    });
+
+
 
 }
 
