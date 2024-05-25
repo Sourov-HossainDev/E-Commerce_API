a2d2244e659112af9027bd6f81e3cd071f6c4c29
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-26 00:01:54 +0600

    Login portion done

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,7 +1,33 @@
+const emailValidation = require("../helpers/emailValidation");
+const UserList = require("../models/userSchema");
+const bcrypt = require('bcrypt');
 
-function loginController(req,res){
+async function loginController(req,res){
     const {email,password} = req.body;
-    
+
+    if(!email){
+        return res.json({error: 'Email is required'})
+    }else if(!emailValidation(email)){
+        return res.json({error: 'Email is not valid'})
+    }else if(!password){
+        return res.json({error: 'Password is required'})
+    }else{
+        const isEmailExist =await UserList.find({email})
+
+        if(isEmailExist.length > 0){
+            bcrypt.compare(password, isEmailExist[0].password).then(function(result) {
+              if(result){
+                res.json({success: 'Login successfully done'})
+              }else{
+                res.json({error: 'Password is not match'})
+              }
+            });
+        }else{
+            return res.json({error: 'Email is not match'})
+        }
+       
+
+    }
 }
 
 module.exports = loginController;
