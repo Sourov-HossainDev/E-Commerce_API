178b0219dd2191078bebce981d8e1a481cd6a599
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-27 11:37:18 +0600

    start merchant controller

--- route/api/index.js
+++ route/api/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const  router = express.Router();
 const authRouter = require('./authentication')
 const categoryRouter = require('./category')
+const merchantRouter = require('./merchant')
 
 
 
 router.use('/authentication', authRouter);
-router.use('/category', categoryRouter)
+router.use('/category', categoryRouter);
+router.use('/merchant', merchantRouter)
 
 module.exports = router;
---
+++ route/api/merchant.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/becomemerchant', )
+
+
+module.exports = router;
