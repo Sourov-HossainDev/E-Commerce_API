030e17a7b6fa827fa488820b324ae9bbc87549d2
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-27 18:01:33 +0600

    create product

--- controller/merchantController.js
+++ controller/merchantController.js
@@ -4,7 +4,20 @@ const UserList = require("../models/userSchema")
 async function becomemerchant(req, res) {
     const { storeName, officialEmail, officialPhone, address, owner, products } = req.body
     console.log(storeName, officialEmail, officialPhone, address, owner, products);
-
+    
+    if (!storeName) {
+        return res.send({ error: 'Store Name is required' })
+    }
+    if (!officialEmail) {
+        return res.send({ error: 'Official email is required' })
+    }
+    if (!officialPhone) {
+        return res.send({ error: 'Official phone is required' })
+    }
+    if (!address) {
+        return res.send({ error: 'Address is required' })
+    }
+    
     const store = new Store({
         storeName,
         officialEmail,
