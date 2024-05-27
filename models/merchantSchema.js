32eb5a0eb9451c33605026e063dee75243c394df
Sourov-HossainDev <sourovhossain2326@gmail.com>
2024-05-27 12:05:58 +0600

    merchant schema protion done

--- models/merchantSchema.js
+++ models/merchantSchema.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const storeSchema = new Schema({
+    storeName:{
+        type: String,
+        required: true
+    },
+    officialEmail: {
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true 
+    },
+    owner:{
+       type: Schema.Types.ObjectId,
+       ref: "UserList"
+    },
+    products:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Product"
+        }
+    ]
 
 })
+
 
 module.exports = mongoose.model("Store", storeSchema)
